8fc32d07aad061a369bd255285dbaab5e8219da1
Adhityayusuf <adhitya.yusuf1206@gmail.com>
2024-09-26 08:02:04 +0700

    tambah

--- PemilihanBilangan13.java
+++ PemilihanBilangan13.java
@@ -6,13 +6,8 @@ public class PemilihanBilangan13 {
         System.out.print("Masukan Sebuah angka: ");
         int angka = sc.nextInt();
 
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        String jenisBilangan = (angka % 2 == 0) ? " genap " : " ganjil ";
+        System.out.println("Angka " + angka + " termasuk bilangan " + jenisBilangan);
+        
     }
 }
